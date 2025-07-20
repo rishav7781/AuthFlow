2c7c0c18095fee812f3a0742376d8683ff034395
Rishav <guptarishav49@gmail.com>
2025-07-20 23:15:14 +0530

    Cleaned up duplicate root-level backend files

--- db.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require("pg");
-require("dotenv").config();
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-module.exports = pool;
-// This code sets up a connection pool to a PostgreSQL database using the 'pg' library.
--- tokenUtils.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-exports.generateToken = (user) => {
-  return jwt.sign(
-    { id: user.id, mobile: user.mobile },
-    process.env.JWT_SECRET,
-    { expiresIn: process.env.JWT_EXPIRES_IN }
-  );
-};
