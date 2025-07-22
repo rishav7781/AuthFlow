fc445c1282393423e22e7a095dc330eb2e820844
Rishav <guptarishav49@gmail.com>
2025-07-22 16:20:28 +0530

    a

--- frontend/script.js
+++ frontend/script.js
@@ -43,7 +43,8 @@ function showLoading(show = true) {
 async function apiRequest(endpoint, method = 'GET', data = null) {
   const options = { method,
     headers: { 'Content-Type': 'application/json' },
-   // credentials: 'include' };
+   // credentials: 'include'
+    };
   if (data) options.body = JSON.stringify(data);
   try {
     const res = await fetch(`${API_BASE_URL}${endpoint}`, options);
